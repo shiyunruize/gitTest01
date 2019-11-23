8a54e9120868782e048129f3aada6348be5e39a7
syrz <2294204794@qq.com>
2019-11-23 14:45:40 +0800

    添加01

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * @author :ruize
+ * @date : 2019/11/23
+ * @descript:
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
